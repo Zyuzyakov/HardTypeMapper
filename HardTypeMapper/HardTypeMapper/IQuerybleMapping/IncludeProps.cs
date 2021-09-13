f8c055bed5ff03889fff5836c2884ac23ddfc171
Zyuzyakov <Zyuzyakov.pavel@mail.ru>
2021-09-13 16:32:54 +1000

    IQuerybleMApping add

---
+++ HardTypeMapper/HardTypeMapper/IQuerybleMapping/IncludeProps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardTypeMapper.IQuerybleMapping
+{
+    // Информация о заинклуженном свойстве
+    public class IncludeProps
+    {
+        public Type ClassInclude { get; set; } // Тип класса в который заинклужено
+        public string PropertyInclude { get; set; } // Свойство в которое заинклужено
+        public Type TypeInclude { get; set; } // Тип инклюда (тип свойства в которое заинклюжено)
+
+        public IncludeProps() { }
+
+        public IncludeProps(Type classInclude)
+        {
+            ClassInclude = classInclude;
+        }
+
+        public IncludeProps(Type classInclude, string propertyInclude, Type typeInclude) : this(classInclude)
+        {
+            TypeInclude = typeInclude;
+            PropertyInclude = propertyInclude;
+        }
+
+        public IncludeProps SetPropertyInclude(string propertyInclude)
+        {
+            var newIncludeInfo = Clone();
+
+            newIncludeInfo.PropertyInclude = propertyInclude;
+
+            return newIncludeInfo;
+        }
+
+        public IncludeProps Clone()
+        {
+            return new IncludeProps(ClassInclude, PropertyInclude, TypeInclude);
+        }
+    }
+}
---
+++ HardTypeMapper/HardTypeMapper/IQuerybleMapping/QueryMappingInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace HardTypeMapper.IQuerybleMapping
+{
+    class QueryMappingInfo
+    {
+        public List<IncludeProps> IncludeProps { get; set; } // что было заинклюжено настоящими инклудами
+        public IncludeProps CurrentInfoInclude { get; set; } // в текущем мапере что инклудится и куда
+        public bool MapQuery { get; set; } = false; // изначально мапится квери
+        public bool? OnlyNotDeleted { get; set; } // параметр deleted расспространяется на все заинклуженные сущности
+
+        public QueryMappingInfo(List<IncludeProps> infoIncludes, IncludeProps currentInfoInclude, bool mapNotQuery, bool? onlyNotDeleted)
+            : this(currentInfoInclude, mapNotQuery, onlyNotDeleted)
+        {
+            IncludeProps = infoIncludes;
+        }
+
+        public QueryMappingInfo(IncludeProps currentInfoInclude, bool mapNotQuery, bool? onlyNotDeleted)
+        {
+            IncludeProps = new List<IncludeProps>();
+
+            MapQuery = mapNotQuery;
+            CurrentInfoInclude = currentInfoInclude;
+            OnlyNotDeleted = onlyNotDeleted;
+        }
+
+        public QueryMappingInfo SetPropertyInclude(string propertyInclude)
+        {
+            var retObj = Clone();
+            retObj.CurrentInfoInclude.PropertyInclude = propertyInclude;
+
+            return retObj;
+        }
+
+        public QueryMappingInfo Clone()
+        {
+            var cloneThis = new QueryMappingInfo(IncludeProps, CurrentInfoInclude.Clone(), MapQuery, OnlyNotDeleted);
+
+            return cloneThis;
+        }
+    }
+}
