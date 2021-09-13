5fdee905dd7b38aafebcd26afa671186f19a4e76
Zyuzyakov <Zyuzyakov.pavel@mail.ru>
2021-09-13 16:00:07 +1000

    add test CollectionRules

---
+++ HardTypeMapper/UnitTests/CollectionRulesMethodTests/CollectionRulesTests.cs
@@ -0,0 +1,17 @@
+﻿using Interfaces;
+using Xunit;
+using HardTypeMapper;
+
+namespace UnitTests.CollectionRulesMethodTests
+{
+    public class CollectionRulesTests
+    {
+        [Fact]
+        public void VoidConstructor_Correct()
+        {
+            ICollectionRules collectionRules = new CollectionRules();
+
+            Assert.NotNull(collectionRules);
+        }
+    }
+}
