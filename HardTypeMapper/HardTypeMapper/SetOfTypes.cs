5a69f2283b0da5162bdcb244975e94e6189eefe4
Zyuzyakov <Zyuzyakov.pavel@mail.ru>
2021-09-14 14:34:34 +1000

    SetOfTypes.Equals + tests

--- HardTypeMapper/HardTypeMapper/SetOfTypes.cs
+++ HardTypeMapper/HardTypeMapper/SetOfTypes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HardTypeMapper
 {
@@ -12,7 +13,7 @@ namespace HardTypeMapper
             _inTypes.Add(inType);
         }
 
-        public SetOfTypes(string nameRule = null, params Type[] inTypes)
+        public SetOfTypes(string nameRule, params Type[] inTypes)
         {
             SetName = nameRule;
             _inTypes = new HashSet<Type>();
@@ -39,6 +40,31 @@ namespace HardTypeMapper
         public Type GetOutTypeParam()
         {
             return typeof(TOutType);
+        }
+
+        public override bool Equals(object obj)
+        {          
+            if (!(obj is SetOfTypes<TOutType>))
+                return false;
+
+            var equalsObj = (SetOfTypes<TOutType>)obj;
+
+            if (InTypes.ToList().Count != equalsObj.InTypes.ToList().Count)
+                return false;
+
+            if (SetName != equalsObj.SetName)
+                return false;
+
+            if (GetOutTypeParam() != equalsObj.GetOutTypeParam())
+                return false;
+
+            var equalsObjInTypes = equalsObj.InTypes.ToList();
+
+            foreach (var itemIn in InTypes)
+                if (!equalsObjInTypes.Contains(itemIn))
+                    return false;
+
+            return true;
         }
     }
 }
--- HardTypeMapper/UnitTests/CollectionRulesMethodTests/SetOfTypesTests.cs
+++ HardTypeMapper/UnitTests/CollectionRulesMethodTests/SetOfTypesTests.cs
@@ -1,4 +1,5 @@
 ﻿using HardTypeMapper;
+using System.Linq;
 using Xunit;
 
 namespace UnitTests.CollectionRulesMethodTests
@@ -6,20 +7,68 @@ namespace UnitTests.CollectionRulesMethodTests
     public class SetOfTypesTests
     {
         [Fact]
-        public void SetOfTypes_Init_Correct()
+        public void SetOfTypes_VoidConstructor_Correct()
         {
             var setOfTypes1 = new SetOfTypes<object>();
 
             Assert.Empty(setOfTypes1.InTypes);
             Assert.True(string.IsNullOrEmpty(setOfTypes1.SetName));
+            Assert.Equal(typeof(object), setOfTypes1.GetOutTypeParam());
 
             var setOfTypes2 = new SetOfTypes<object>();
 
             Assert.Empty(setOfTypes2.InTypes);
             Assert.True(string.IsNullOrEmpty(setOfTypes2.SetName));
-
+            Assert.Equal(typeof(object), setOfTypes1.GetOutTypeParam());
 
             Assert.True(setOfTypes1.Equals(setOfTypes2));
+        }
+
+        [Fact]
+        public void SetOfTypes_ParamConstructor_Correct()
+        {
+            var setOfTypes1 = new SetOfTypes<Street>(typeof(StreetDto));
+
+            Assert.Single(setOfTypes1.InTypes);
+            Assert.True(string.IsNullOrEmpty(setOfTypes1.SetName));
+            Assert.Equal(typeof(Street), setOfTypes1.GetOutTypeParam());
+
+            var setOfTypes2 = new SetOfTypes<Street>(typeof(StreetDto), "test");
+
+            Assert.Single(setOfTypes2.InTypes);
+            Assert.Equal("test", setOfTypes2.SetName);
+            Assert.Equal(typeof(Street), setOfTypes1.GetOutTypeParam());
+
+            Assert.False(setOfTypes1.Equals(setOfTypes2));
+        }
+
+        [Fact]
+        public void SetOfTypes_MultiInParams_Correct()
+        {
+            var setOfTypes1 = new SetOfTypes<Street>("", typeof(StreetDto), typeof(HouseDto));
+
+            Assert.Equal(2, setOfTypes1.InTypes.ToList().Count);
+            Assert.True(string.IsNullOrEmpty(setOfTypes1.SetName));
+            Assert.Equal(typeof(Street), setOfTypes1.GetOutTypeParam());
+        }
+
+        [Fact]
+        public void Equals_MultiInParams_Correct()
+        {
+            var setOfTypes1 = new SetOfTypes<Street>("", typeof(StreetDto), typeof(HouseDto));
+
+            Assert.Equal(2, setOfTypes1.InTypes.ToList().Count);
+            Assert.True(string.IsNullOrEmpty(setOfTypes1.SetName));
+            Assert.Equal(typeof(Street), setOfTypes1.GetOutTypeParam());
+
+            var setOfTypes2 = new SetOfTypes<Street>("", typeof(HouseDto), typeof(StreetDto));
+
+            Assert.Equal(2, setOfTypes2.InTypes.ToList().Count);
+            Assert.True(string.IsNullOrEmpty(setOfTypes2.SetName));
+            Assert.Equal(typeof(Street), setOfTypes2.GetOutTypeParam());
+
+            Assert.True(setOfTypes1.Equals(setOfTypes2));
+            Assert.True(setOfTypes2.Equals(setOfTypes1));
         }
     }
 }
