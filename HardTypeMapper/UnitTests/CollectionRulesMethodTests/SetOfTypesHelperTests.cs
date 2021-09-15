aac8ea9b591b3dba40942d7cac0260e857c4d8e8
Zyuzyakov <Zyuzyakov.pavel@mail.ru>
2021-09-15 15:18:18 +1000

    usings clear

--- HardTypeMapper/UnitTests/CollectionRulesMethodTests/SetOfTypesHelperTests.cs
+++ HardTypeMapper/UnitTests/CollectionRulesMethodTests/SetOfTypesHelperTests.cs
@@ -1,9 +1,5 @@
 ﻿using HardTypeMapper.Models.CollectionModels;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnitTests.TestModels;
 using Xunit;
 
