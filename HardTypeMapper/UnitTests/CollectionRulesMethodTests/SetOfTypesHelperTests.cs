1ec7aa1f6fa436a89544d40ed741070fbed402af
Zyuzyakov <Zyuzyakov.pavel@mail.ru>
2021-09-15 15:17:09 +1000

    add tests SetOfTypesHelper

---
+++ HardTypeMapper/UnitTests/CollectionRulesMethodTests/SetOfTypesHelperTests.cs
@@ -0,0 +1,37 @@
+﻿using HardTypeMapper.Models.CollectionModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTests.TestModels;
+using Xunit;
+
+namespace UnitTests.CollectionRulesMethodTests
+{
+    public class SetOfTypesHelperTests
+    {
+        [Fact]
+        public void Create_WhenInParamsNullOrZero_ThrowArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => SetOfTypesHelper.Create<StreetDto>(string.Empty, null));
+        }
+
+        [Fact]
+        public void Create_Correct()
+        {
+            var setOfTypes = SetOfTypesHelper.Create<StreetDto>("test", typeof(Street), typeof(House));
+
+            Assert.NotNull(setOfTypes);
+
+            Assert.Equal("test", setOfTypes.SetName);
+            Assert.Equal(typeof(StreetDto), setOfTypes.GetOutTypeParam());
+          
+            var inParams = setOfTypes.GetInTypeParams();
+
+            Assert.Equal(2, inParams.Length);
+            Assert.Contains(typeof(Street), inParams);
+            Assert.Contains(typeof(House), inParams);
+        }
+    }
+}
