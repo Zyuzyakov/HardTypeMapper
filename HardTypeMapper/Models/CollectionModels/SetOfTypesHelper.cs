184b60dfc30026293d776461fc8cc4a65076bbaf
Zyuzyakov <Zyuzyakov.pavel@mail.ru>
2021-09-15 15:07:20 +1000

    change text exception

--- HardTypeMapper/Models/CollectionModels/SetOfTypesHelper.cs
+++ HardTypeMapper/Models/CollectionModels/SetOfTypesHelper.cs
@@ -7,7 +7,7 @@ namespace HardTypeMapper.Models.CollectionModels
         public static SetOfTypes<TOutType> Create<TOutType>(string nameRule, params Type[] inTypes)
         {
             if (inTypes is null || inTypes.Length < 1)
-                throw new ArgumentException($"Количество входных типов должно быть >= 1.");
+                throw new ArgumentException($"Количество входных типов должно быть > 0.");
 
             return new SetOfTypes<TOutType>(nameRule, inTypes);
         }
