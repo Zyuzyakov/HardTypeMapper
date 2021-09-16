8536209af101dbc221af329b971559e1b46eb930
Zyuzyakov <Zyuzyakov.pavel@mail.ru>
2021-09-17 09:50:12 +1000

    error test

--- HardTypeMapper/UnitTests/HardMapperTests/HardMapper_MapCollection_Tests.cs
+++ HardTypeMapper/UnitTests/HardMapperTests/HardMapper_MapCollection_Tests.cs
@@ -86,6 +86,39 @@ namespace UnitTests.HardMapperTests
             house2.Flats = new List<Flat>() { flat2, flat3 };
         }
 
+        private void Init2()
+        {
+            collectionRules = new CollectionRules();
+
+            collectionRules.AddRule<Flat, FlatDto>((mm, flat) => new FlatDto()
+            {
+                Name = flat.Name,
+                HouseDto = mm.Map<House, HouseDto>(flat.House)
+            });
+
+            collectionRules.AddRule<House, HouseDto>((mm, house) => new HouseDto()
+            {
+                Name = house.Name,
+                StreetDto = mm.Map<Street, StreetDto>(house.Street),
+                FlatsDto = mm.Map<Flat, FlatDto>(house.Flats).ToList()
+            });
+
+            hardMapper = new HardMapper(collectionRules);
+
+            house = new House()
+            {
+                Name = "house",
+            };
+
+            flat = new Flat()
+            {
+                Name = "flat",
+                House = house,
+            };
+
+            house.Flats = new List<Flat>() { flat };
+        }
+
         [Fact]
         public void Map_FromFlatCollection_Correct()
         {
@@ -93,9 +126,9 @@ namespace UnitTests.HardMapperTests
 
             var listFlats = new List<Flat>() { flat, flat2, flat3 };
 
-            var flatsDtos = hardMapper.Map<Flat, FlatDto>(listFlats);
+            var flatsDtos = hardMapper.Map<Flat, FlatDto>(listFlats).ToList();
 
-            Assert.Equal(3, flatsDtos.Count());
+            Assert.Equal(3, flatsDtos.Count);
 
             var flat1Dto = flatsDtos.First(x => x.Name == "flat");
             var flat2Dto = flatsDtos.First(x => x.Name == "flat2");
@@ -118,6 +151,25 @@ namespace UnitTests.HardMapperTests
             Assert.NotNull(flat3Dto.HouseDto.StreetDto);
             Assert.Empty(flat3Dto.HouseDto.FlatsDto);
             Assert.Empty(flat3Dto.HouseDto.StreetDto.HousesDto);
+        }
+
+        [Fact]
+        public void Map_FromHouseCollection_Correct()
+        {
+            Init2();
+
+            var listHouses = new List<House>() { house };
+
+            var housesDtos = hardMapper.Map<House, HouseDto>(listHouses).ToList();
+
+            Assert.Equal(1, housesDtos.Count);
+
+            var house1Dto = housesDtos.First(x => x.Name == "house");
+
+            Assert.NotNull(house1Dto);
+            Assert.Single(house1Dto.FlatsDto);
+            var house1Flat = house1Dto.FlatsDto.First();
+            Assert.Null(house1Flat.HouseDto);
         }
     }
 }
