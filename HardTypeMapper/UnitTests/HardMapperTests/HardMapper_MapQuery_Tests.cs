c64391234f969dec58b8414cec30a3ccff7b8e58
Zyuzyakov <Zyuzyakov.pavel@mail.ru>
2021-09-28 17:11:03 +1000

    tests add HardMapper_MapQuery_Tests

---
+++ HardTypeMapper/UnitTests/HardMapperTests/HardMapper_MapQuery_Tests.cs
@@ -0,0 +1,88 @@
+﻿using HardTypeMapper;
+using HardTypeMapper.CollectionRules;
+using Interfaces.CollectionRules;
+using Interfaces.MapMethods;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnitTests.TestModels;
+
+namespace UnitTests.HardMapperTests
+{
+    public class HardMapper_MapQuery_Tests
+    {
+        private DbContextOptions<TestContext> _options;
+
+        private ICollectionRules collectionRules;
+        private IMapMethods hardMapper;
+
+        Street street;
+        House house;
+        Flat flat;
+
+        private void Init()
+        {
+            collectionRules = new CollectionRules();
+
+            collectionRules.AddRule<Flat, FlatDto>((mm, flat) => new FlatDto()
+            {
+                Name = flat.Name,
+                HouseDto = mm.Map<House, HouseDto>(flat.House)
+            });
+
+            collectionRules.AddRule<House, HouseDto>((mm, house) => new HouseDto()
+            {
+                Name = house.Name,
+                StreetDto = mm.Map<Street, StreetDto>(house.Street),
+                FlatsDto = mm.Map<Flat, FlatDto>(house.Flats).ToList()
+            });
+
+            collectionRules.AddRule<Street, StreetDto>((mm, street) => new StreetDto()
+            {
+                Name = street.Name,
+                HousesDto = mm.Map<House, HouseDto>(street.Houses).ToList()
+            });
+
+            hardMapper = new HardMapper(collectionRules);
+
+
+            street = new Street()
+            {
+                Name = "street",
+            };
+
+            house = new House()
+            {
+                Name = "house",
+                Street = street,
+            };
+
+            flat = new Flat()
+            {
+                Name = "flat",
+                House = house,
+            };
+
+            street.Houses = new List<House>() { house };
+            house.Flats = new List<Flat>() { flat };
+        }
+
+        private void SetupDb(string dbName)
+        {
+            Init();
+
+            _options = new DbContextOptionsBuilder<TestContext>()
+                   .UseInMemoryDatabase(databaseName: $"{nameof(HardMapper_MapQuery_Tests)} {Guid.NewGuid()} {dbName}")
+                   .Options;         
+
+            using (var context = new TestContext(_options))
+            {
+                // adds
+
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ HardTypeMapper/UnitTests/TestModels/TestContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UnitTests.TestModels
+{
+    public class TestContext : DbContext
+    {
+        public TestContext(DbContextOptions<TestContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Street> Streets { get; set; }
+        public virtual DbSet<House> Houses { get; set; }
+        public virtual DbSet<Flat> Flats { get; set; }
+    }
+}
